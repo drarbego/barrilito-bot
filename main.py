c52d2d9b8f41833ad488b41e4fbc5eb5295c52b5
Alejandro <alejandro@Alejandros-MacBook-Pro.local>
2020-03-16 10:24:29 -0600

    added basic response logic

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import falcon
 import json
+import requests
+
+from os import environ
+
+API_TOKEN = environ.get('API_TOKEN')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+
+
+def respond(channel_id):
+    headers = {
+      'Authorization': f'Bearer {API_TOKEN}',
+      'Content-Type': 'application/json;charset=UTF-8',
+    }
+    requests.post(
+        'https://slack.com/api/chat.postMessage',
+        headers=headers,
+        json={
+            'text': 'YIII',
+            channel: channel_id, 
+        }
+    )
 
 
 class MessageResource:
@@ -9,6 +30,32 @@ class MessageResource:
         resp.body = json.dumps({
             "status": "ok"
         })
+
+    def on_post(self, req, res):
+        body = req.body
+
+        if 'challenge' in body:
+            return res.status = falcon.HTTP_200
+
+        if 'token' not in body or body['token'] != BOT_TOKEN:
+            return res.status = falcon.HTTP_401
+    
+        event = body['event']
+
+        event_type = event['type']
+        event_subtype = event['subtype']
+        message_text = event['text']
+        channel_id = event['channel']
+
+        if event_subtype == 'bot_message':
+            return res.status = falcon.HTTP_200
+
+        if eventType != 'app_mention':
+            return
+
+        respond(channel_id);
+        return res.status = falcon.HTTP_200
+
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
