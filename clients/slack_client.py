bcca390b59bf48cba68166dc171a6dc22e3ddd2f
3D <drarbego@gmail.com>
2020-11-07 15:15:04 -0600

    oops

--- clients/slack_client.py
+++ clients/slack_client.py
@@ -4,8 +4,8 @@ SLACK_DOMAIN = 'https://slack.com'
 
 class SlackClient:
     def __init__(self, slack_api_token, bot_token):
-        self._slack_api_token = configuration.get["SLACK_API_TOKEN"]
-        self._bot_token = configuration.get("BOT_TOKEN")
+        self._slack_api_token = slack_api_token
+        self._bot_token = bot_token
 
     def send_message(self, channel_id, text):
         url = f"{SLACK_DOMAIN}/api/chat.postMessage"
